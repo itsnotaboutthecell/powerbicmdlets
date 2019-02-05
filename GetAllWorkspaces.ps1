d361809e8bfd39423ad1fbe04bd057b463ff2b22
Alex Michael Powers <44716363+itsnotaboutthecell@users.noreply.github.com>
2019-02-04 21:13:21 -0600

    Create GetAllWorkspaces.ps1

---
+++ GetAllWorkspaces.ps1
@@ -0,0 +1,11 @@
+Connect-PowerBIServiceAccount
+
+# Exports Power BI Workspace Information
+Set-Variable -Name OutPath -Value C:\Temp\
+$workspaces = Get-PowerBIWorkspace -All
+
+Export-Csv -InputObject $workspaces -NoTypeInformation -Path "$($OutPath)workspaces.csv"
+
+Write-Host "Total Number of Workspaces: $($workspaces.Count)`n"
+Disconnect-PowerBIServiceAccount
+Read-Host "Press any key when complete."
