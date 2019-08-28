24081f1a675a9f3be9e648cbe29b4b2b88048f0c
Alex Michael Powers <44716363+itsnotaboutthecell@users.noreply.github.com>
2019-08-28 16:24:52 -0500

    Create ExportAllReports.ps1

---
+++ ExportAllReports.ps1
@@ -0,0 +1,17 @@
+# Exports all Power BI reports within the current organization
+# If you only want to export the reports you have access to change to -Scope Individual
+
+Login-PowerBIServiceAccount
+
+Get-PowerBIReport -Scope Individual | Export-Csv "C:\temp\Power BI\pbi_reports.csv"
+
+$pbiReports = Get-PowerBIReport -Scope Organization
+
+ForEach ($Result in $pbiReports) {
+
+    Write-Host "Report Id: $($Result.Id) - Report Name: $($Result.Name)"
+    Export-PowerBIReport -Id $($Result.Id) -OutFile "C:\temp\Power BI\$($Result.Name).pbix"
+
+}
+
+Logout-PowerBIServiceAccount
